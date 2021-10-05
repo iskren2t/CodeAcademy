6245c7b75fe862f9c82f9d4cf452a07f59710a8a
Iskren Todorov <iskren.2t@abv.bg>
2021-10-05 19:08:22 +0300

    homework2

--- Homework_2/hw2task17.c
+++ Homework_2/hw2task17.c
@@ -4,12 +4,12 @@
 
 int main()
 {
-    char arr[6] = {'1','0','1','0','1','0'};
+    char arr[10] = "1010110100";
     int count = 0;
 
-    for(int i = 0; i < 6; i++)
+    for(int i = 0; i < 10; i++)
     {
-        if(arr[i] == 0 && arr[i+1] == 1 && arr[i+2] == 0);
+        if(arr[i] == '0' && arr[i+1] == '1' && arr[i+2] == '0')
         {
             count++;
         }
